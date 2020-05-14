9159c72e1bf572bf309974018de299bde3968f2e
mariadeor <41467566+mariadeor@users.noreply.github.com>
2020-05-14 19:47:30 +0200

    Update predictMalaria.m

--- predictMalaria.m
+++ predictMalaria.m
@@ -1,17 +1,20 @@
-function [predictions,imds] = predictMalaria(sourceFolder, snn, cnn)
-    % This function classifies RBCs of the smears in the sourceFolder into
-    % malaria parasitized or not.
+% Copyright (c) 2020 Maria Delgado-Ortet (github.com/mariadeor)
+function [predictions,imds] = predictMalaria(folder, snn, cnn)
+    
+    % This function classifies masked and cropped RBCs from the wide field blood smears
+    % into malaria parasitized or not.
     % Inputs: 
-    % 1. sourceFolder
+    % 1. folder: contains the wide field smears images.
     % 2. snn: Semantic Neural Network that will segment the RBCs from the
             % smears.
     % 3. cnn: Convolutional Neural Network that classifies each of the RBCs
             % into malaria parasitized or not.
     
     tic
-    extractRBC(sourceFolder,snn);
-    [~,foldername] = fileparts(sourceFolder);
+    extractRBC(folder,snn);
+    [~,foldername] = fileparts(folder);
     imds = imageDatastore([pwd,'/rbcs_',foldername,'_mask']);
     predictions = classify(cnn, imds);
     toc
+
 end
